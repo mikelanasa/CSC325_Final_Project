50cae6021e54de923664343c4413c989e48a14b8
mikelanasa <mikelanasa@gmail.com>
2023-05-05 06:41:09 -0400

    Added functionality to display data from firestore in menu

--- src/main/java/modelview/MenuController.java
+++ src/main/java/modelview/MenuController.java
@@ -4,6 +4,15 @@
  */
 package modelview;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.cloud.FirestoreClient;
 import com.mycompany.mvvmexample.App;
 import models.MenuItem;
 import viewmodel.MenuItemView;
@@ -11,8 +20,13 @@ import viewmodel.MenuItemView;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import static java.lang.Double.parseDouble;
+import static java.lang.String.valueOf;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
@@ -33,6 +47,8 @@ public class MenuController {
 
     @FXML
     private Button AddtoOrder;
+    
+    List<MenuItem> menuItems = new ArrayList<MenuItem>();
 
     @FXML
     private void switchToPrimary() throws IOException {
@@ -47,15 +63,17 @@ public class MenuController {
     @FXML
     void initialize() throws FileNotFoundException {
         List<MenuItem> menuItems = new ArrayList<MenuItem>();
-
-       
-        for (int i = 0; i < 3; i++) {
-            Image image = new Image(getClass().getResourceAsStream("/com/mycompany/mvvmexample/bakerylogo.png"));
-            MenuItem NewItem = new MenuItem("Cake", 20.0, "sdsdsd", image);
-            menuItems.add(NewItem);
-        }
-        MenuDisplay(menuItems);
-
+        
+        
+            for (int i = 0; i < 3; i++) {
+                
+                menuItems.add(getMenuItems().get(i));
+                //Image image = new Image(getClass().getResourceAsStream("/com/mycompany/mvvmexample/bakerylogo.png"));
+                //MenuItem NewItem = new MenuItem(name, price, description, image);
+                //menuItems.add(NewItem);  
+            }
+            MenuDisplay(menuItems);
+            
     }
 
     private void MenuDisplay(List<MenuItem> menuItems) {
@@ -69,6 +87,35 @@ public class MenuController {
             //AddtoOrder = new Button("Add to Order");
 
         }
+    }
+    
+    private List<MenuItem> getMenuItems() {
+        
+        //List<MenuItem> menuItems = new ArrayList<MenuItem>();
+        
+        try {
+            Firestore db = FirestoreClient.getFirestore();
+            // Create a reference to the cities collection
+            //CollectionReference menuItem = db.collection("MenuItem");
+            
+            // asynchronously retrieve all documents
+            ApiFuture<QuerySnapshot> future = db.collection("MenuItem").get();
+            // future.get() blocks on response
+            List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+            for (QueryDocumentSnapshot document : documents) {
+                Image image = new Image(document.get("image").toString());
+                String name = document.get("name").toString();
+                double price = parseDouble(document.get("price").toString());
+                String description = document.get("description").toString();
+                
+                MenuItem m = new MenuItem(name, price, description, image);
+                menuItems.add(m);
+            }           
+        } catch (InterruptedException | ExecutionException ex) {
+            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return menuItems;
     }
 
 }
