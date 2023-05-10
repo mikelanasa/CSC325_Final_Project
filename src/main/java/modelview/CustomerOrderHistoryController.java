bb5fb1537c1b8f4861c1aa36d6da53318c51c976
mikelanasa <mikelanasa@gmail.com>
2023-05-09 22:43:19 -0400

    Added functionality to view orders

--- src/main/java/modelview/CustomerOrderHistoryController.java
+++ src/main/java/modelview/CustomerOrderHistoryController.java
@@ -4,13 +4,31 @@
  */
 package modelview;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.cloud.FirestoreClient;
 import com.mycompany.mvvmexample.App;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import static modelview.SigninController.currentUser;
 
 /**
  * FXML Controller class
@@ -33,5 +51,53 @@ public class CustomerOrderHistoryController {
     private void shutDownApp() {
         Platform.exit();
     }   
+    
+    @FXML
+    private ListView viewOrderListView;
+    
+    List<String> emails = new ArrayList<String>();
+    
+    //static UserRecord currentUser;
+    
+    @FXML
+    private void showOrders() { 
+        
+        List<String> showEmails = getOrders();
+        for(int i = 0; i < showEmails.size(); i++) {
+            viewOrderListView.getItems().add(showEmails.get(i));
+        }     
+        
+    }
+    
+    private List<String> getOrders() {
+        CountDownLatch latch = new CountDownLatch(1);
+        
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> future = db.collection("Orders").whereEqualTo("email", currentUser.getEmail()).get();
+
+        future.addListener(() -> {
+            try {
+                List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+                for (QueryDocumentSnapshot document : documents) {
+                    String email =  "email: " + document.get("email").toString() + ", item: " + document.get("item0").toString();
+                    
+                    emails.add(email);
+                }
+            } catch (InterruptedException | ExecutionException ex) {
+                Logger.getLogger(CustomerBakeryMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                latch.countDown(); // Signal that the task is complete
+            }
+        }, ForkJoinPool.commonPool()); // Use the common fork-join pool as the executor
+
+        try {
+            latch.await(); // Wait for the task to complete
+        } catch (InterruptedException e) {
+            Logger.getLogger(MenuController.class.getName()).log(Level.SEVERE, null, e);
+            Thread.currentThread().interrupt();
+        }
+
+        return emails;
+    }
     
 }
