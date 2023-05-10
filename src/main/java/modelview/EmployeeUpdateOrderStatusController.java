08b8026584a5c9474176539ca9123e15f27688ff
erickxcruz <85849149+erickxcruz@users.noreply.github.com>
2023-05-09 22:59:11 -0400

    Update Order Employees

--- src/main/java/modelview/EmployeeUpdateOrderStatusController.java
+++ src/main/java/modelview/EmployeeUpdateOrderStatusController.java
@@ -4,13 +4,24 @@
  */
 package modelview;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.WriteResult;
+import com.google.firebase.cloud.FirestoreClient;
 import com.mycompany.mvvmexample.App;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -19,21 +30,74 @@ import javafx.fxml.Initializable;
  */
 public class EmployeeUpdateOrderStatusController {
 
-     @FXML
+    @FXML
+    private TextField orderIdTextField;
+    @FXML
+    private TextField statusUpdateTextField;
+
+    @FXML
     private void switchToPrimary() throws IOException {
         App.setRoot("logInPage.fxml");
     }
-    
+
     @FXML
     private void switchToHome() throws IOException {
         App.setRoot("EmployeeMenu.fxml");
     }
-    
-    
+
     @FXML
     private void shutDownApp() {
         Platform.exit();
     }
-    
-    
+
+    @FXML
+    private void updateOrderStatus() throws ExecutionException {
+        String orderId = orderIdTextField.getText();
+        String updatedStatus = statusUpdateTextField.getText(); // Assuming you have a TextField named "statusTextField"
+
+        if (orderId.isBlank() || orderId.isEmpty()) {
+            showAlert("Enter a valid order id");
+        } else if (updatedStatus.isBlank() || updatedStatus.isEmpty()) {
+            showAlert("Enter a valid status message");
+        } else {
+            // Retrieve the document from Firestore
+            DocumentReference docRef = FirestoreClient.getFirestore().collection("Orders").document(orderId);
+            ApiFuture<DocumentSnapshot> future = docRef.get();
+
+            try {
+                // Get the document snapshot
+                DocumentSnapshot document = future.get();
+
+                if (document.exists()) {
+                    // Document with the provided ID exists
+
+                    // Update the order status field
+                    Map<String, Object> updates = new HashMap<>();
+                    updates.put("status", updatedStatus);
+
+                    ApiFuture<WriteResult> updateResult = docRef.update(updates);
+                    updateResult.get();
+                    // Perform further actions or display a confirmation message
+                    showAlert("Order with ID " + orderId + " exists. Order status updated...");
+
+                    // Additional actions...
+                } else {
+                    // Document with the provided ID does not exist
+                    showAlert("Order with ID " + orderId + " does not exist.");
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                // Error occurred while retrieving the document
+                showAlert("Error occurred while updating the order status. Please try again.");
+            }
+
+        }
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, message, ButtonType.OK);
+        alert.setTitle("Order ID");
+        alert.setHeaderText(null);
+        alert.showAndWait();
+    }
+
 }
